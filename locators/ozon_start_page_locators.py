01e696410450b90ab349ac48299c7b0d7422c0cc
lambotik <lambotik1988@gmail.com>
2023-11-09 21:17:31 +0300

    Added: Did a refactoring code.

--- locators/ozon_start_page_locators.py
+++ locators/ozon_start_page_locators.py
@@ -2,8 +2,21 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 
 class OzonPageLocators:
-    search = (AppiumBy.ACCESSIBILITY_ID, 'ru.ozon.app.android:id/searchTv')
+    search = (AppiumBy.ID, 'ru.ozon.app.android:id/searchTv')
     HANDLER_VIEW = (AppiumBy.ACCESSIBILITY_ID, 'HandlerView')
     MAIN = (AppiumBy.ACCESSIBILITY_ID, 'Главная')
     CATALOG = (AppiumBy.ACCESSIBILITY_ID, "Каталог")
     ADVERTISING = (AppiumBy.ID, 'ru.ozon.app.android:id/touch_outside')
+    LATER = (AppiumBy.ID, 'ru.ozon.app.android:id/remindLater')
+    FRAME = (AppiumBy.ID, 'ru.ozon.app.android:id/design_bottom_sheet')
+    BALL = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="мяч футбольный"]/android.widget.TextView')
+    CATALOG_SEARCH = (AppiumBy.ID, 'ru.ozon.app.android:id/searchEt')
+    SELECT_BALL = (AppiumBy.XPATH,
+                   '(//android.widget.FrameLayout[@content-desc="grid"])[1]'
+                   '/android.view.ViewGroup/android.widget.FrameLayout')
+    PRICE = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="25,20 BYN"]/android.widget.TextView')
+    TITLE = (AppiumBy.ID, 'ru.ozon.app.android:id/btnTitleTv')
+    CART = (AppiumBy.ID, 'ru.ozon.app.android:id/cartBtn')
+    BADGE = (AppiumBy.ID, 'ru.ozon.app.android:id/tab_badge')
+    GO_IN_CART = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="Корзина"]/android.widget.ImageView')
+    PRICE_IN_CART = (AppiumBy.ID, 'ru.ozon.app.android:id/footerPriceTv')
--- pages/ozon_start_page.py
+++ pages/ozon_start_page.py
@@ -6,5 +6,22 @@ class OzonStartPage(BasePage):
     locators = OzonPageLocators()
 
     def check(self):
-        x = self.element_is_presence(self.locators.CATALOG)
-        x.click()
+        self.element_is_presence(self.locators.LATER).click()
+        self.element_is_presence(self.locators.FRAME)
+        self.driver.back()
+        self.element_is_presence(self.locators.CATALOG).click()
+        search = self.element_is_presence(self.locators.search)
+        search.click()
+        self.element_is_presence(self.locators.CATALOG_SEARCH).send_keys('мяч')
+        self.element_is_presence(self.locators.BALL).click()
+        self.element_is_presence(self.locators.SELECT_BALL).click()
+        self.element_is_presence(self.locators.TITLE)
+        self.driver.swipe(start_y=1400, start_x=500, end_y=700, end_x=500)
+        price = self.element_is_presence(self.locators.PRICE).text
+        print('Price on market:', price)
+        self.element_is_presence(self.locators.CART).click()
+        assert self.element_is_presence(self.locators.BADGE)
+        self.element_is_presence(self.locators.GO_IN_CART).click()
+        price_in_cart = self.element_is_presence(self.locators.PRICE_IN_CART).text
+        print('Price in cart:', price_in_cart)
+        return price, price_in_cart
