73d46a16faeb479bdcc244a4891db2a7fbff189d
lambotik <lambotik1988@gmail.com>
2023-11-09 21:21:15 +0300

    Added: Did a refactoring code.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,6 @@
 from appium.webdriver.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.wait import WebDriverWait as wait
+from selenium.webdriver.support.wait import WebDriverWait as Wait
 
 
 class BasePage(WebDriver):
@@ -9,5 +9,5 @@ class BasePage(WebDriver):
         self.timeout = 10
 
     def element_is_presence(self, locator):
-        return wait(self.driver, self.timeout).until(ec.presence_of_element_located(locator),
+        return Wait(self.driver, self.timeout).until(ec.presence_of_element_located(locator),
                                                      message=f'Element not found by locator: {locator}')
