dfac565d5043556e77c02edb5d371ca226bc8872
lambotik <lambotik1988@gmail.com>
2023-11-09 21:57:51 +0300

    Added: test_get_settings

--- tests/test_ozon.py
+++ tests/test_ozon.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from pages.ozon_start_page import OzonStartPage
 
 
@@ -8,3 +10,20 @@ class TestOzonMarket:
         start_page = OzonStartPage(driver)
         price_market, price_in_cart = start_page.check()
         assert price_market == price_in_cart, 'Critical bag!!!'
+
+    def test_get_settings(self,driver):
+        """
+        This method gets device info
+        :param driver:
+        :return:
+        """
+        settings = driver.get_settings()
+        battery_info = driver.battery_info
+        capabilities = driver.capabilities
+        device_time = driver.device_time
+        print('Battery_info:', battery_info)
+        print('\nCapabilities:')
+        pprint(capabilities)
+        print('\ndevice_time',device_time)
+        print('\nsettings')
+        pprint(settings)
